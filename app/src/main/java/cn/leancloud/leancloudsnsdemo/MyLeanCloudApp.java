d8ccab3f09ccde72743c4f435d47bc9082eb5f08
xsui <284898541@qq.com>
2019-08-20 15:54:04 +0800

    update java-unified-sdk to v5.0.21.

--- app/src/main/java/cn/leancloud/leancloudsnsdemo/MyLeanCloudApp.java
+++ app/src/main/java/cn/leancloud/leancloudsnsdemo/MyLeanCloudApp.java
@@ -2,8 +2,10 @@ package cn.leancloud.leancloudsnsdemo;
 
 import android.app.Application;
 
-import com.avos.avoscloud.AVOSCloud;
 import com.mob.MobSDK;
+
+import cn.leancloud.AVLogger;
+import cn.leancloud.AVOSCloud;
 
 /**
  * Created by BinaryHB on 2018/5/11.
@@ -15,9 +17,9 @@ public class MyLeanCloudApp extends Application {
   public void onCreate() {
     super.onCreate();
 
+    AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
     // 初始化参数依次为 this, AppId, AppKey
     AVOSCloud.initialize(this, "I94is3iS8CzyLsY4lmYz2HjT-gzGzoHsz", "yDObYfeTC9Y0e1aqT7nrWJgu");
-    AVOSCloud.setDebugLogEnabled(true);
 
     MobSDK.init(this);
 
